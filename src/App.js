47a168149d4a3bc6742883a771fb5ca030dc6e39
Marcus Hunter <Marcusj94@sbcglobal.net>
2018-08-16 16:12:07 -0700

    landing page topics
    css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import MyRoutes from './config/routes';
+import TopicContainer from './containers/TopicContainer';
 
 class App extends Component {
 
@@ -14,6 +15,7 @@ class App extends Component {
       <div className="App">
         <NavBar loggedIn={this.state.loggedIn} />
         { MyRoutes }
+        <TopicContainer />
       </div>
     );
   }
