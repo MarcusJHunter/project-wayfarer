63bd2119870b29e156883ddda5d8d5516ae890e5
t10chap <tevinrawls@gmail.com>
2018-08-16 12:12:03 -0700

    Updated profile

---
+++ src/components/Profile.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Profile extends Component {
+    render(){
+        return(
+            <div>
+                <h1>UserName</h1>
+                <h2>User City</h2>
+            </div>
+        )
+    }
+}
