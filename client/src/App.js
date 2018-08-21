14ded545672333f5fd95899b44c22484993e04e0
Marcus Hunter <Marcusj94@sbcglobal.net>
2018-08-21 15:43:27 -0700

    profile merge

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 
   state = {
     loggedIn: false,
-    user: {}
+    user: {},
   }
 
 
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -10,26 +10,25 @@ class Profile extends Component {
         let email = window.localStorage.getItem('user')
 
 
-        Users.getProfile(email)
-            .then(response => {
-                if (response.data == "Good Call") {
-                    user = response
-                    console.log(user)
-                } else {
-                    console.log("user not found")
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                console.log("In catch");
-            });
+        // Users.getProfile(email)
+        //     .then(response => {
+        //         if (response.data === "Good Call") {
+        //             user = response
+        //             console.log(user)
+        //         } else {
+        //             console.log("user not found")
+        //         }
+        //     })
+        //     .catch(err => {
+        //         console.log(err);
+        //         console.log("In catch");
+        //     });
 
-
+        console.log(this.props.user);
         return(
             <div className='profile'>
-                <h3 clsssName='profileName'>Email: {user.email} </h3>
+                <h3 className='profileName'>Email: {this.props.user} </h3>
                 <h4 className='profileCity'>City:{user.location}</h4>
-                <h4 className='profileSignUpDate'>SUD:</h4>
             </div>
         )
     }
